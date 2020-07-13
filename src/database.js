b110449a80b8ec7bd3802fbcb3825a6134c26011
danicelistobon <danielct-97@hotmail.com>
2020-07-12 19:50:03 -0500

    changed variable

--- src/database.js
+++ src/database.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI
-  ? process.env.MONGODB_URI
-  : 'mongodb://localhost/database-test';
+const URI = process.env.MONGODB_URI;
 
 mongoose.connect(URI, {
   useNewUrlParser: true,
