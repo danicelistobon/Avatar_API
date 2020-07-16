cb94f4eb15d5a2b66facec9c652c3215c3ef25a6
danicelistobon <danielct-97@hotmail.com>
2020-07-15 23:51:39 -0500

    created user model

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+  id: { type: Number, trim: true, required: true },
+  name: { type: String, trim: true, required: true },
+  email: { type: String, trim: true, required: true },
+  password: { type: String, trim: true, required: true }
+});
+
+userSchema.methods.encryptPwd = async password => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+userSchema.methods.matchPwd = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('Users', userSchema);
