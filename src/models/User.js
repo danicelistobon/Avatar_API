e5c60194a5b00d15d36ae5964957c8a2b29066da
danicelistobon <danielct-97@hotmail.com>
2020-07-16 20:52:18 -0500

    modified user model

--- src/models/User.js
+++ src/models/User.js
@@ -5,17 +5,17 @@ const bcrypt = require('bcryptjs');
 const userSchema = new Schema({
   id: { type: Number, trim: true, required: true, unique: true },
   name: { type: String, trim: true, required: true },
-  email: { type: String, trim: true, required: true },
+  email: { type: String, trim: true, required: true, unique: true },
   password: { type: String, trim: true, required: true }
 });
 
-userSchema.methods.encryptPwd = async password => {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
+userSchema.methods.encryptPwd = password => {
+  const salt = bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
 };
 
-userSchema.methods.matchPwd = async function (password) {
-  return await bcrypt.compare(password, this.password);
+userSchema.methods.matchPwd = function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('Users', userSchema);
